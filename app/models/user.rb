13349deb383f622b478e3e545cb0d8152c3b2db0
Ziv Zamechek <ziv921@yahoo.com>
2016-06-22 14:31:20 -0400

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase! }
+
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -8,4 +9,6 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/new'
 
+  resources :users
+  
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help', as: 'helf'
   get  '/about',   to: 'static_pages#about'
